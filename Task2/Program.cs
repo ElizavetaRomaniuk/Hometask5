3af551a90b7cac42184bc66c18a06e5d8df4fb4d
Elizaveta Romaniuk <elizavetasiniuk@gmail.com>
2023-02-27 13:23:17 +0300

    Task 2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int sum = 0;
+int[] arrayResult = new int[4];
+int[] GetArray()
+{
+    int[] array = new int[4];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-20, 20);
+    return array;
+}
+static void SumPositionNotEven (int[] array, int sum)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+        sum = sum + array[i];
+        }
+    }
+    Console.WriteLine(sum);
+}
+void PrintResult(int[] array)
+{
+    Console.WriteLine($"Array result is {String.Join(" ", array)}");
+}
+arrayResult = GetArray();
+PrintResult(arrayResult);
+SumPositionNotEven(arrayResult, sum);
